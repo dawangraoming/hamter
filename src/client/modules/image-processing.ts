cad2dccd78872a9c37962733dac7586dad97768e
dawangraoming <514266254@qq.com>
2018-07-18 14:38:54 +0800

    注释无用代码

--- src/client/modules/image-processing.ts
+++ src/client/modules/image-processing.ts
@@ -3,7 +3,7 @@
  * @date 2018/7/8
  */
 
-import * as Jimp from 'jimp';
+// import * as Jimp from 'jimp';
 import {stat, createReadStream, createWriteStream, writeFile} from 'fs';
 import {promisify} from 'util';
 import * as fileType from 'file-type';
@@ -66,25 +66,25 @@ export async function convertPsdToPng(sourcePath: string, writePath: string): Pr
  * @param {string} sourcePath
  * @return {Promise<GetImageDataReturn>}
  */
-export async function getImageData(sourcePath: string): Promise<GetImageDataReturn> {
-  // use jimp to get the image pixel's width and height
-  const pixel = Jimp.read(sourcePath).then(lenna => {
-    return {
-      width: lenna.bitmap.width,
-      height: lenna.bitmap.height
-    };
-  });
-  // use promisify to change node callback to promise
-  const sizePromise = promisify(stat)(sourcePath);
-  const data = await Promise.all([sizePromise, pixel]);
-
-  return {
-    size: data[0].size,
-    time: Math.floor(data[0].birthtimeMs),
-    width: data[1].width,
-    height: data[1].height
-  };
-}
+// export async function getImageData(sourcePath: string): Promise<GetImageDataReturn> {
+//   // use jimp to get the image pixel's width and height
+//   const pixel = Jimp.read(sourcePath).then(lenna => {
+//     return {
+//       width: lenna.bitmap.width,
+//       height: lenna.bitmap.height
+//     };
+//   });
+//   // use promisify to change node callback to promise
+//   const sizePromise = promisify(stat)(sourcePath);
+//   const data = await Promise.all([sizePromise, pixel]);
+//
+//   return {
+//     size: data[0].size,
+//     time: Math.floor(data[0].birthtimeMs),
+//     width: data[1].width,
+//     height: data[1].height
+//   };
+// }
 
 /**
  * copy file to other path;
@@ -109,16 +109,16 @@ export function fileCopy(sourcePath: string, writePath: string): Promise<any> {
  * @param {string} writePath
  * @return {Promise<Jimp.Jimp>}
  */
-export function generateThumb(sourcePath: string | Promise<Jimp>, writePath: string): Promise<Jimp> {
-  let process;
-  // polymorphism params, support string path or promise object
-  if (typeof sourcePath === 'string') {
-    process = Jimp.read(sourcePath as string);
-  } else {
-    process = sourcePath;
-  }
-  // use jipm method to change the image size and quality and then write to file;
-  return process.then(lenna => {
-    return lenna.resize(480, Jimp.AUTO).quality(60).write(writePath);
-  });
-}
+// export function generateThumb(sourcePath: string | Promise<Jimp>, writePath: string): Promise<Jimp> {
+//   let process;
+//   // polymorphism params, support string path or promise object
+//   if (typeof sourcePath === 'string') {
+//     process = Jimp.read(sourcePath as string);
+//   } else {
+//     process = sourcePath;
+//   }
+//   // use jipm method to change the image size and quality and then write to file;
+//   return process.then(lenna => {
+//     return lenna.resize(480, Jimp.AUTO).quality(60).write(writePath);
+//   });
+// }
